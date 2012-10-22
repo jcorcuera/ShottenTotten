93d414a3e4491d0ddafc80936b6bcb5bb50c973a
José Corcuera <jzcorcuera@gmail.com>
2012-10-22 01:04:33 -0500

    Refactor cusersView addUser code

--- app/assets/javascripts/app/modules/user_module.js
+++ app/assets/javascripts/app/modules/user_module.js
@@ -28,18 +28,22 @@ var UsersView = Backbone.View.extend({
     this.collection.on('add', this.addUser, this);
   },
 
-  addUser: function(user) {
-    if (this.collection.length == 1) {
-      this.$el.html(game.template('waiting_oponent', {}));
-    } else if (this.collection.length == 2) {
-      this.$el.html('');
+  addUser: function(user, collection, options) {
+    //TODO: refactor code to use render? or nested views.
+    if (options.index == 1) {
+      this.render();
+    }
+  },
+
+    render: function() {
       var userView1 = new UserView({model: this.collection.at(0)});
       var userView2 = new UserView({model: this.collection.at(1)});
+      this.$el.html('');
       this.$el.append(userView1.render().el);
       this.$el.append("<span class='block'>VS</div>");
       this.$el.append(userView2.render().el);
+      return this;
     }
-  }
 
 });
 
